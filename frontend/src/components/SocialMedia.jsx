14f447618568b63c48efc3ef1f00243755c3b084
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-03-05 20:11:40 +0530

    Portfolio Template Done

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-      <div>
+      <a href='https://twitter.com/KanojiaKarnik' target='_blank' rel='noreferrer'>
         <BsTwitter />
-      </div>
-      <div>
-        <FaFacebookF />
-      </div>
-      <div>
+      </a>
+      <a href="https://github.com/karnikkanojia" target='_blank' rel='noreferrer'>
+        <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/karnik.kanojia/" target='_blank' rel='noreferrer'>
         <BsInstagram />
-      </div>
+      </a>
     </div>
   )
 }

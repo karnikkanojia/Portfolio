fa205db98edf48148ec8d0e16624806e95b43420
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-03-11 12:22:41 +0530

    File Structure Modified

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+      <a href='https://twitter.com/KanojiaKarnik' target='_blank' rel='noreferrer'>
+        <BsTwitter />
+      </a>
+      <a href="https://github.com/karnikkanojia" target='_blank' rel='noreferrer'>
+        <FaGithub />
+      </a>
+      <a href="https://www.instagram.com/karnik.kanojia/" target='_blank' rel='noreferrer'>
+        <BsInstagram />
+      </a>
+    </div>
+  )
+}
+
+export default SocialMedia

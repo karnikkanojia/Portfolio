ca759caefc5569344ec752309f747a3559245a23
Karnik Kanojia <karnikk1406120@gmail.com>
2023-07-07 00:18:58 +0530

    Fix: Clear directory

--- container/index.js
+++
@@ -1,9 +0,0 @@
-export { default as About } from './about/About';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Skills } from './skills/Skills';
-export { default as Testimonial } from './testimonial/Testimonial';
-export { default as Work } from './work/Work';
-
-
-

a922a5e50762ca0b217f68220dfb936943975f6a
Karnik Kanojia <karnikk1406120@gmail.com>
2023-07-25 23:41:13 +0530

    Change: Resume link

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Karnik Kanojia',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://www.karnikkanojia.tech', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@KanojiaKarnik',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Karnik Kanojia',
+        short_name: 'Karnik Kanojia',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
